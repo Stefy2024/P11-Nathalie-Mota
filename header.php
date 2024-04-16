f39b9f8baed576032cffb0e540dc8549e76bf9a8
Stefy <stef.tilip@gmail.com>
2024-04-16 16:13:19 +0200

    travail sur la modale

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="fr">
     <head>
+        
+        
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Nathalie Mota</title>
@@ -9,13 +11,36 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Space+Mono:ital,wght@0,400;1,400;1,700&display=swap" rel="stylesheet">
 
+        <?php wp_head(); ?>
     </head>
 
 <body>
-<div>
-    <img class="img_logo" src="<?php echo get_template_directory_uri(); ?>.\assets\images/logo.png" alt="logo Nathalie Mota" />
-</div>    
+   
+    <div class="nav_bar">
+        <img class="mota_logo" src="<?php echo get_template_directory_uri(); ?>.\assets\images\logo.png" alt="logo Nathalie Mota" />
 
-<nav id="navigation">
-    <?php wp_nav_menu(array('theme_location' => 'main')); ?>
-</nav>
+        <nav id="navigation">
+            <?php wp_nav_menu(
+                array(
+                    'theme_location' => 'main')); ?>
+        </nav>
+    </div>
+
+
+    <!-- The Modal -->
+<div id="myModal" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">
+  <span class="close">x</span>
+  <div class="mota-form-container">
+        <?php
+        echo do_shortcode('[contact-form-7 id="0e5d643" title="Formulaire de contact modal"]')
+        ?>
+    </div>
+</div>
+<!-- <script src="./wp-content/themes/NathalieMota/js/scripts.js"></script> -->
+</div>
+
+    
+
