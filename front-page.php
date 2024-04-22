21fad59dcbcae59ebc9903b09a2bf830e876debd
Stefy <stef.tilip@gmail.com>
2024-04-22 15:37:17 +0200

    modif single.php pour affichage photo

--- front-page.php
+++ front-page.php
@@ -5,6 +5,10 @@ get_header();
     <img class="hero_img" src="<?php echo get_template_directory_uri(); ?>.\assets\images\nathalie-0.jpeg" alt="fête d'un mariage" />
     <img class="hero_title" src="<?php echo get_template_directory_uri(); ?>.\assets\images\hero_title.png" alt="photographe event" />
 </div>
+
+<?php get_template_part('single');
+?>
+
 <?php 
 get_footer(); 
 ?>
