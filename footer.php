389e29bbce6a521070789eef770f7e6361d07b6d
Stefy <stef.tilip@gmail.com>
2024-05-23 11:09:18 +0200

    css mobile footer

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
     <?php wp_nav_menu(array('theme_location' => 'footer')); ?>
     <p class="footer_text">Tous droits réservés</p>
 </nav>
+
+
     <!-- Modal contact-->
     <?php get_template_part('template-parts/modal/contact');?>
     <!-- lightbox-->
